865d6666f45047c0378b90f6f1144b39105a350d
farkmu45 <farukmicros@outlook.com>
2021-12-02 19:24:41 +0700

    Add responsive styles

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -11,9 +11,10 @@ import {
   Alert,
   Linking,
 } from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 
 function goToUnity(link) {
-  Linking.openURL(link).catch(err => {
+  Linking.openURL(link).catch(() => {
     Alert.alert('Error', 'Harap install aplikasi AR untuk DScanner', [
       {
         text: 'OK',
@@ -36,40 +37,50 @@ export default function HomeScreen({navigation}) {
         barStyle="dark-content"
       />
 
-      <View style={{flexGrow: 1, width: '100%', padding: 20}}>
-        <Text
+      <ScrollView style={{width: '100%'}}>
+        <View
           style={{
-            color: 'black',
-            fontSize: 40,
-            letterSpacing: -1,
-            marginTop: 30,
-            fontFamily: 'Inter-Bold',
+            padding: 20,
           }}>
-          DScanner
-        </Text>
-        <Text
-          style={{
-            lineHeight: 23,
-            color: 'black',
-            fontSize: 18,
-            marginTop: 12,
-            fontFamily: 'Inter-Medium',
-          }}>
-          Welcome to DScanner App by Project D and F2!
-        </Text>
-        <AnimatedLottieView
-          source={require('./assets/cube.json')}
-          autoPlay
-          loop
-          style={{marginTop: 40}}
-        />
-      </View>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 40,
+              letterSpacing: -1,
+              marginTop: 30,
+              fontFamily: 'Inter-ExtraBold',
+            }}>
+            DScanner
+          </Text>
+          <Text
+            style={{
+              lineHeight: 23,
+              color: 'black',
+              fontSize: 18,
+              marginTop: 12,
+              fontFamily: 'Inter-Medium',
+            }}>
+            Welcome to DScanner App by Project D and F2!
+          </Text>
+          <AnimatedLottieView
+            source={require('./assets/cube.json')}
+            autoPlay
+            loop
+            style={{alignSelf: 'center', width: 390}}
+            resizeMode="cover"
+          />
+        </View>
+      </ScrollView>
 
       <View
         style={{
           width: '100%',
           height: 70,
           backgroundColor: '#86B4A9',
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          display: 'flex',
           alignItems: 'center',
         }}>
         <View style={{marginTop: -30, flexDirection: 'row'}}>
